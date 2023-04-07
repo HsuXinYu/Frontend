e000213c2de8bb7f4406270197872a01ebfdd263
xinyu <jolin7180732@gmail.com>
2023-04-07 10:40:01 +0800

    20230407

--- JAVASCRIPT/1.js
+++ JAVASCRIPT/1.js
@@ -1,14 +1,11 @@
-// 只修改這一行下面的代碼
-function rangeOfNumbers(startNum, endNum) {
-  if (startNum === endNum) {
-    console.log(startNum);
-    return [startNum];
-  } else {
-    var range = rangeOfNumbers(startNum, endNum - 1);
-    range.push(endNum);
-    console.log(range);
-    return range;
+function titleCase(str) {
+  var arr = str.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1).toLowerCase();
+    // console.log(arr[i]);
   }
+  arr = arr.join(" ");
+  console.log(arr);
 }
-// 只修改這一行上面的代碼
-rangeOfNumbers(1, 5);
+
+titleCase("I'm a little tea pot");
