3acc11066cc5909bc052c68a580ffa2749380ce2
xinyu <jolin7180732@gmail.com>
2023-06-07 14:34:21 +0800

    20230607

--- practice/snake.js
+++ practice/snake.js
@@ -9,22 +9,24 @@ const row = canvas.height / unit;
 let myGame = setInterval(draw, 150);
 
 let snake = [];
-snake[0] = {
-  x: 60,
-  y: 0,
-};
-snake[1] = {
-  x: 40,
-  y: 0,
-};
-snake[2] = {
-  x: 20,
-  y: 0,
-};
-snake[3] = {
-  x: 0,
-  y: 0,
-};
+function createSnake() {
+  snake[0] = {
+    x: 60,
+    y: 0,
+  };
+  snake[1] = {
+    x: 40,
+    y: 0,
+  };
+  snake[2] = {
+    x: 20,
+    y: 0,
+  };
+  snake[3] = {
+    x: 0,
+    y: 0,
+  };
+}
 
 class Fruit {
   constructor() {
@@ -36,10 +38,41 @@ class Fruit {
     ct.fillStyle = "orange";
     ct.fillRect(this.x, this.y, unit, unit);
   }
+
+  pickLocation() {
+    let overlapping = false;
+    let new_x;
+    let new_y;
+
+    do {
+      new_x = Math.floor(Math.random() * col) * unit;
+      new_y = Math.floor(Math.random() * row) * unit;
+      console.log("果實新座標", new_x, new_y);
+      checkOverlap(new_x, new_y);
+    } while (overlapping);
+
+    function checkOverlap(new_x, new_y) {
+      for (let i = 0; i < snake.length; i++) {
+        if (new_x == snake[i].x && new_y == snake[i].y) {
+          console.log("overlapping...");
+          overlapping = true;
+          return;
+        } else {
+          overlapping = false;
+        }
+      }
+    }
+
+    this.x = new_x;
+    this.y = new_y;
+  }
 }
 
+//初始設定
+createSnake();
 let myFruit = new Fruit();
 
+//按下按鍵變更蛇移動方向
 window.addEventListener("keydown", changeDirection);
 let dir = "right";
 function changeDirection(e) {
@@ -53,9 +86,21 @@ function changeDirection(e) {
   } else if (e.key == "ArrowRight" && dir != "left") {
     dir = "right";
   }
+
+  //防止連續點擊按鍵導致問題
+  window.removeEventListener("keydown", changeDirection);
 }
 
 function draw() {
+  //確認蛇是否咬到自己
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[0]["x"] == snake[i]["x"] && snake[0]["y"] == snake[i]["y"]) {
+      clearInterval(myGame);
+      alert("遊戲結束!");
+      return;
+    }
+  }
+
   // console.log("test");
   //畫布
   ct.fillStyle = "black";
@@ -90,6 +135,7 @@ function draw() {
       snake[i]["y"] = canvas.height - unit;
     }
   }
+
   //移動蛇
   let snakeX = snake[0]["x"];
   let snakeY = snake[0]["y"];
@@ -108,12 +154,14 @@ function draw() {
     y: snakeY,
   };
 
-  //蛇是否吃到果實
+  //確認蛇是否吃到果實
   if (snake[0]["x"] == myFruit["x"] && snake[0]["y"] == myFruit["y"]) {
-    console.log("eating");
+    myFruit.pickLocation();
+    myFruit.drawFruit();
   } else {
     snake.pop();
   }
 
   snake.unshift(newHead);
+  window.addEventListener("keydown", changeDirection);
 }
