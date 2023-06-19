b52ccdfb6a045c94a18f53e2655044530fc4cdc3
xinyu <jolin7180732@gmail.com>
2023-06-19 13:48:26 +0800

    20230619

---
+++ JAVASCRIPT/js/fetch.js
@@ -0,0 +1,34 @@
+// let fetchPromise = fetch(
+//   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+// );
+
+// console.log(fetchPromise);
+
+// fetchPromise.then((response) => {
+//   // let jsonObject = response.json(); //async function
+//   // console.log(jsonObject);
+
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+// fetchPromise
+//   .then((response) => response.json()) //可一起省略{}及return
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+let fetchPromise = fetch(
+  "https://javascript/apis/fetching-data/can-store/products.json"
+);
+
+fetchPromise
+  .then((response) => response.json()) //可一起省略{}及return
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log(fetchPromise);
+    console.log(e);
+  });
